b362cd6b4d90f60cde4bd8ad2bcb25af924c1df8
subhashnaidu77 <55914212+subhashnaidu77@users.noreply.github.com>
2026-07-03 15:30:10 +0530

    Implement basic login server with token generation

---
+++ server.js
@@ -0,0 +1,30 @@
+const http = require('node:http');
+const server = http.createServer((request,response)=>{
+   if(request.url==='/login'&& request.method==='POST'){
+    let data = ""
+
+    request.on('data',(chunk)=>{
+        data +=chunk;
+    })
+
+    request.on('end',()=>{
+        const user = JSON.parse(data);
+
+        if(user.username==='student' && user.password==='1234'){
+            const token = 'SECRET_TOKEN'+ Math.random();
+            response.writeHead(200,{'Content-Type':'application/json'});
+            response.end(JSON.stringify({message:'login done',token:token}));
+        }
+        else{
+            response.writeHead(401,{'Content-Type':'application/json'});
+            response.end(JSON.stringify({message:'invalid credentials'}));
+        }
+    })
+   } else {
+       response.writeHead(404, {'Content-Type': 'application/json'});
+       response.end(JSON.stringify({message: 'not found'}));
+   }
+})
+server.listen(3000,()=>{
+    console.log("server is running on port 3000");
+})
